a71f3ea639775aa67f9909f04dfa68f579c43689
Serhii <serhiisharyi77@gmail.com>
2023-05-07 18:10:47 +0300

    feat: Added spirograph drawing shape

--- helpers.py
+++ helpers.py
@@ -20,3 +20,11 @@ def move_toward_random_direction(turtle, directions):
     random_direction = random.choice(directions)
     turtle.right(random_direction)
     turtle.forward(100)
+
+
+def draw_spirograph(turtle, gap_size, colors=None):
+    for _ in range(round(360 / gap_size)):
+        if colors:
+            turtle.color(random.choice(colors))
+        turtle.setheading(turtle.heading() + gap_size)
+        turtle.circle(100)
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from constants import COLORS, DIRECTIONS
 from turtle import Screen, Turtle
-from helpers import move_toward_random_direction
+from helpers import draw_spirograph
 import random
 
 tim = Turtle()
 tim.shape('turtle')
-tim.pensize(15)
 tim.speed('fastest')
+draw_spirograph(turtle=tim, gap_size=5, colors=COLORS)
+
 screen = Screen()
-
-for _ in range(50):
-    tim.color(random.choice(COLORS))
-    move_toward_random_direction(tim, DIRECTIONS)
-
 screen.exitonclick()
