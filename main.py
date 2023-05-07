48841c476aa81e23123a513aad37b6f03ab18eaa
Serhii <serhiisharyi77@gmail.com>
2023-05-07 18:12:10 +0300

    feat: Code refactor

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from constants import COLORS, DIRECTIONS
 from turtle import Screen, Turtle
 from helpers import draw_spirograph
-import random
 
 tim = Turtle()
 tim.shape('turtle')
 tim.speed('fastest')
 draw_spirograph(turtle=tim, gap_size=5, colors=COLORS)
-
 screen = Screen()
 screen.exitonclick()
